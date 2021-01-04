17b0354402eb0c1f40e85d0f8157884a1c107899
saskasaska <oldstep13@gmail.com>
2021-01-04 23:14:48 +0200

    add main logic

---
+++ index.js
@@ -0,0 +1,69 @@
+const title = document.querySelector("#title");
+const content = document.querySelector("#content");
+const addBtn = document.querySelector("#addBtn");
+const noteForm = document.querySelector("#note_form");
+const colorPicker = document.querySelector("#color_picker");
+const rootDiv = document.querySelector("#root");
+
+const LS_KEY = 'notes';
+const defaultNotes = JSON.parse(localStorage.getItem(LS_KEY));
+let notes = defaultNotes || [];
+let deleteBtns;
+
+const onDeleteClick = (e) => {
+	deleteNote(e.target.dataset.id);
+}
+
+const renderNotes = () => {rootDiv.innerHTML = `${notes.map(
+(note) =>
+`<div style="background-color: ${note.color};" class= "rendered_note">
+	<div>${note.createDate}</div>
+	<div>${note.title}</div>
+	<div>${note.content}</div>
+	<button class="delete_button" data-id="${note.id}">delete</button>
+</div>
+`
+).join("")}`;
+deleteBtns = document.querySelectorAll(".delete_button");
+deleteBtns.forEach((btn) => btn.addEventListener("click", onDeleteClick));
+renderedNotes = document.querySelectorAll(".rendered_note");
+};
+
+
+renderNotes();
+
+const addNote = () => {
+	const note = {
+		id: "" + Date.now(),
+		title: title.value,
+		content: content.value,
+		createDate: new Date(),
+		color : colorPicker.value
+	}
+	
+	notes.push(note);
+	localStorage.setItem(LS_KEY, JSON.stringify(notes));
+	renderNotes();
+}
+
+const deleteNote = (id) => {
+	notes = notes.filter(item => item.id !== id);
+	localStorage.setItem(LS_KEY, JSON.stringify(notes));
+	renderNotes();
+	deleteBtns.forEach((btn) => btn.addEventListener("click", onDeleteClick));
+}
+
+
+
+const changeColor = () => {
+	noteForm.style.backgroundColor = colorPicker.value;
+}
+
+// const renderedNoteColor = () => {
+// 	renderedNotes.style.backgroundColor = note.color;
+// }
+
+
+deleteBtns.forEach((btn) => btn.addEventListener("click", onDeleteClick));
+addBtn.addEventListener("click", addNote);
+colorPicker.addEventListener("input", changeColor);
